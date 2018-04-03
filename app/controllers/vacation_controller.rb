c4762856bf4afd0bfacb6507500a5f61f251f4c9
Tyler Carey Pruyn <Doppleganger@tyler-pruyns-imac.local>
2018-04-03 14:34:45 -0400

    added users controller

--- app/controllers/vacation_controller.rb
+++
@@ -1,2 +0,0 @@
-class VacationController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'users/auth/twitter/callback', to: 'sessions#create'
+
+  resources :users do
+    resources :vacations
+  end
   #delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
