74fc093c9d382b3b86d68d7883ce5c42e9e87c93
Tyler Carey Pruyn <Doppleganger@tyler-pruyns-imac.local>
2018-04-03 14:45:07 -0400

    reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :vacations
+  post '/vacations/new'
   root 'welcome#index'
-  get 'users/auth/twitter/callback', to: 'sessions#create'
+  #get 'users/auth/twitter/callback', to: 'sessions#create'
 
-  resources :users do
-    resources :vacations
-  end
+  #resources :users do
+  #  resources :vacations
+  #end
   #delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
