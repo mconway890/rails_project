b03e69ef6d230573c354dab7ce6f43eddf84a850
Michele Conway <Doppleganger@tyler-pruyns-imac.local>
2018-04-04 14:37:54 -0400

    activity methods

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,2 +1,15 @@
 class ActivityController < ApplicationController
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.create(activity_params)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :activity_category, :user_id, :vacation_id)
+  end
 end
