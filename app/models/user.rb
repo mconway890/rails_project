f7ecc18e686637c2317a11bd01d0eae4571b56b0
Michele Conway <Doppleganger@tyler-pruyns-imac.local>
2018-04-09 12:18:09 -0400

    fixed shared link path

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:twitter], :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:twitter]
 
   def self.find_for_oauth(auth)
      find_or_create_by(uid: auth.uid, provider: auth.provider)
