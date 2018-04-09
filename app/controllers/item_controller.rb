a71754aa87c51b68384a8211ac5a5d49ab00f17c
Michele Conway <Doppleganger@tyler-pruyns-imac.local>
2018-04-09 12:59:08 -0400

    item methods

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,2 +1,40 @@
 class ItemController < ApplicationController
+  def index
+      @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to items_path
+  end
+
+  def edit
+    @item = Item.find_by(params[:id])
+  end
+
+  def update
+    @item = Item.find_by(params[:id])
+    @item.update(item_params)
+  end
+
+  def destroy
+    @item = Item.find_by(params[:id])
+    @item.destroy
+    redirect_to root_path, :notice => "Your item was deleted."
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :item_category, :user_id, :vacation_id)
+  end
 end
